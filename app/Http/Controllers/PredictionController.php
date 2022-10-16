a6f7acb1506633d1dfda353904fc5b945fc28ce9
VKonovaluck <vkonovaluck@gmail.com>
2022-10-16 13:38:05 +0300

    upload function

---
+++ app/Http/Controllers/PredictionController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Prediction;
+use Illuminate\Http\Request;
+use File;
+
+class PredictionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('prediction');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if($request->isMethod('post')){
+            if($request->hasFile('files')) {
+
+                $file = $request->file('files');
+                $file->move(public_path() . '/path','prediction.txt');
+                $contents = File::get(public_path() . '/path/prediction.txt');
+                $contents =  explode(".", $contents);
+                $predict = [];
+                foreach ($contents as $content){
+                    $predict[]=['name'=>$content];
+                }
+
+                \DB::table('predictions')->truncate();
+                \DB::table('predictions')->insert($predict);
+                return redirect()->route('home');
+            }
+        };
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
